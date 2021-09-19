f47b3af78dacb428b1c6a72b14ffc853def371eb
Francisco Dionizio Salgado <franciscodsalgado@gmail.com>
2021-09-19 20:50:33 -0300

    Initiate project

---
+++ src/main/java/com/ciskow/coursespringjpahibernate/CourseSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ciskow.coursespringjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringJpaHibernateApplication.class, args);
+	}
+
+}
